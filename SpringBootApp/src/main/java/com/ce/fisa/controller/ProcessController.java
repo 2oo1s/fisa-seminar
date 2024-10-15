48ab053bc3ff645e3f2abc19125e7e34769d471a
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-10-15 19:45:54 +0900

    mod) controller context

--- SpringBootApp/src/main/java/com/ce/fisa/controller/ProcessController.java
+++ SpringBootApp/src/main/java/com/ce/fisa/controller/ProcessController.java
@@ -9,15 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class ProcessController {
 
-//	@GetMapping("/lamda")
-//	public String reqRes() {
-//		log.info("요청 수락 ~~~");
-//		return "Lamda식으로 변화 감지하기 성공!!!"; // 이 메서드는 REST 응답으로 사용할 수 있습니다.
-//	}
-
 	@GetMapping("")
-	public String reqRe2s() {
-		log.info("요청 수락 ~~~");
+	public String reqRes() {
+		log.info("요청이 정상적으로 들어왔습니다!!!");
+		
 		return "index";
 	}
 }
